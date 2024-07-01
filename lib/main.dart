9eb4cd33837853fd5819bb1ad5667f26f070a96c
Atul Vashisht <guruaugust@gmail.com>
2024-07-01 23:33:54 +0530

    listeners bg/fg added for notifications

--- lib/main.dart
+++ lib/main.dart
@@ -8,31 +8,40 @@ import 'package:flutter/material.dart';
 import 'auth_gate.dart';
 import 'firebase_options.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-//   final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
-//
-//   final token = await FirebaseMessaging.instance.getToken();
-//   if (token != null) {
-//     print("token $token");
-//     final user=FirebaseAuth.instance.currentUser;
-//     if(user!=null){
-//
-//       //make firestore operation to update token in users collection
-//       final db = FirebaseFirestore.instance;
-//       final userRef = db.collection("users").doc(user.uid);
-//
-// // Atomically add a new token to the "deviceTokens" array field.
-//       userRef.update({
-//         "deviceTokens": FieldValue.arrayUnion([token]),
-//       });
-//
-//
-//     }
-//   }
+  final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+  final token = await FirebaseMessaging.instance.getToken();
+  if (token != null) {
+    print("token $token");
+    final user=FirebaseAuth.instance.currentUser;
+    if(user!=null){
+
+      //make firestore operation to update token in users collection
+      final db = FirebaseFirestore.instance;
+      final userRef = db.collection("users").doc(user.uid);
+
+// Atomically add a new token to the "deviceTokens" array field.
+      userRef.update({
+        "deviceTokens": FieldValue.arrayUnion([token]),
+      });
+
+
+    }
+  }
 
   // FirebaseMessaging.instance.onTokenRefresh
   //     .listen((fcmToken) {
@@ -44,6 +53,18 @@ void main() async{
   //     .onError((err) {
   //   // Error getting token.
   // });
+  //To handle foreground
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  //To handle background
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const DattaDevsthanPimpalgaon());
 }
