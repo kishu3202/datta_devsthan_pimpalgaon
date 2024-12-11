ce25f4d47ca9d5093a66765929ed3b3a4e064d26
Kishori <p.kishori1995@gmail.com>
2024-12-11 13:20:34 +0530

    dipdann

--- lib/drawerScreen/dipdan_screen.dart
+++ lib/drawerScreen/dipdan_screen.dart
@@ -1,113 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:wakelock/wakelock.dart';
 
 
-//import 'package:video_player/video_player.dart';
-//import 'package:wakelock/wakelock.dart';
-
-class DipdanScreen extends StatelessWidget {
-
+class DipdanScreen extends StatefulWidget {
+  const DipdanScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return SizedBox();
-  }
+  State<DipdanScreen> createState() => _DipdanScreenState();
 }
 
+class _DipdanScreenState extends State<DipdanScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
-// class DipdanScreen extends StatefulWidget {
-//   const DipdanScreen({super.key});
-//
-//   @override
-//   State<DipdanScreen> createState() => _DipdanScreenState();
-// }
+  @override
+  void initState(){
+    super.initState();
+    _controller  = VideoPlayerController.asset('asset/vedio/dipdaanVedio.mp4');
+    _initializeVideoPlayerFuture = _controller.initialize();
+    Wakelock.enable();
+  }
+  @override
+  void dispose(){
+    _controller.dispose();
+    Wakelock.disable();
+    super.dispose();
+  }
 
-// class _DipdanScreenState extends State<DipdanScreen> {
-//   late VideoPlayerController _controller;
-//   late Future<void> _initializeVideoPlayerFuture;
-//
-//   @override
-//   void initState(){
-//     super.initState();
-//     _controller  = VideoPlayerController.asset('asset/vedio/dipdaanVedio.mp4');
-//     _initializeVideoPlayerFuture = _controller.initialize();
-//     Wakelock.enable();
-//   }
-//   @override
-//   void dispose(){
-//     _controller.dispose();
-//     Wakelock.disable();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("दीपदान",style: TextStyle(color: Colors.white,fontSize: 18),),
-//       centerTitle: true,),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: [
-//             Padding(
-//               padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-//               child: InteractiveViewer(
-//                   panEnabled: true,
-//                   minScale: 0.5,
-//                   maxScale: 4.0,
-//                 child: const Image(image: AssetImage("asset/images/dipdan.jpeg"),)),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(10.0),
-//               child: FutureBuilder(
-//                 future: _initializeVideoPlayerFuture,
-//                 builder: (context, snapshot) {
-//                   if (snapshot.connectionState == ConnectionState.done) {
-//                     return AspectRatio(
-//                       aspectRatio: _controller.value.aspectRatio,
-//                       child: VideoPlayer(_controller),
-//                     );
-//                   } else {
-//                     return const Center(child: CircularProgressIndicator());
-//                   }
-//                 },
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.all(10.0),
-//               child: SizedBox(
-//                 height: 45,
-//                 width: MediaQuery.of(context).size.width,
-//                 child: ElevatedButton(
-//                   style: ElevatedButton.styleFrom(
-//                       backgroundColor: Colors.orange,
-//                       shape: RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.circular(10),
-//                       )),
-//                   onPressed: () {
-//                     setState(() {
-//                       if (_controller.value.isPlaying) {
-//                         _controller.pause();
-//                         Wakelock.disable();
-//                       } else {
-//                         _controller.play();
-//                         Wakelock.enable();
-//                       }
-//                     });
-//                   },
-//                 // child: ElevatedButton(onPressed: () {
-//                 //   setState(() {
-//                 //     _controller.value.isPlaying
-//                 //         ? _controller.pause()
-//                 //         : _controller.play();
-//                 //   });
-//                 // },
-//                 child: Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow
-//                 ),
-//                             ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("दीपदान",style: TextStyle(color: Colors.white,fontSize: 18),),
+      centerTitle: true,),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+              child: InteractiveViewer(
+                  panEnabled: true,
+                  minScale: 0.5,
+                  maxScale: 4.0,
+                child: const Image(image: AssetImage("asset/images/dipdan.jpeg"),)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: FutureBuilder(
+                future: _initializeVideoPlayerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                height: 45,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )),
+                  onPressed: () {
+                    setState(() {
+                      if (_controller.value.isPlaying) {
+                        _controller.pause();
+                        Wakelock.disable();
+                      } else {
+                        _controller.play();
+                        Wakelock.enable();
+                      }
+                    });
+                  },
+                child: ElevatedButton(onPressed: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow
+                ),
+                            ),
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,8 @@ import firebase_auth
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
+import video_player_avfoundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -19,4 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
